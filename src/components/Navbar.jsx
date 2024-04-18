4099a09fd767d10f9589901ab8d1e08642d8d846
john ogheneochuko <ochustech56@gmail.com>
2024-04-18 01:33:20 +0100

    added a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import logo from './images/logo.jpg'
+
+import './Navbar.css'
+
+const Navbar = () => {
+
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    const closeMenu = () => setClick(false)
+
+    return (
+        <div className='header'>
+            <nav className='navbar'>
+                <a href='/' className='logo'>
+                    <img className='logo' src={logo} alt='logo' />
+                </a>
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
+                        : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
+
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className='nav-item'>
+                        <a href='/' onClick={closeMenu}>Home</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#about' onClick={closeMenu}>About</a>
+                    </li>
+                   
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
