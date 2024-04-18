866a6ee889dae84f307f19244eca0f6a1a4f4740
john ogheneochuko <ochustech56@gmail.com>
2024-04-18 01:31:04 +0100

    added new changes to the ui

---
+++ src/components/Repos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Repos.css'
+import Profile from './profile'
+
+const Repo = () => {
+    return (
+        <div className='repos'>
+            <div className='content'>
+          <Profile />
+          
+            </div>
+        </div>
+    )
+}
+
+export default Repo
