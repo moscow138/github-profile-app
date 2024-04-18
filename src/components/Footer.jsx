eb08bbbba09b325395360661a9be4c94da0c13c0
john ogheneochuko <ochustech56@gmail.com>
2024-04-18 21:35:08 +0100

    updated the footer and navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,6 @@ const Footer = () => {
     return (
         <div className='footer'>
             <div className='container'>
-                <ul>
-                    <li className='nav-item'>
-                        <a href='/'>Home</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href='#about'>About</a>
-                    </li>
-                  
-                </ul>
                 <div className='bottom'>
                     <span className='line'></span>
                     <p>2024, Inc. All rights reserved</p>
