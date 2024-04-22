baa8057458621f9f3777e0a1fcfd0db5b15dca21
JOHN OGHENEOCHUKO <ochustech56@gmail.com>
2024-04-22 20:09:46 +0100

    uppdate

--- src/pages/Viewrepo.jsx
+++ src/pages/Viewrepo.jsx
@@ -1,23 +1,64 @@
-import React from "react";
+import React, {useEffect, useState,} from "react";
 import { Link, useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./Viewrepo.css"
-
+import { ChakraBaseProvider } from "@chakra-ui/react";
+import { Stack,
+    Text,
+    Box,
+    StackDivider,
+    List,
+    ListItem,
+    ListIcon,
+    OrderedList,
+    UnorderedList,
+   
+    } from '@chakra-ui/react'
 
 
 const Viewrepo = () => {
+// data state
+const [word, setword] = useState([]);
 
-    const {repoId} = useParams();
+useEffect(()=>{
+    fetch('https://api.github.com/users/moscow138/repos')
+    .then((response) => response.json())
+    .then((repo) => {
+     setword(repo)
+
+    
+    
+    });
+    
+},[])
+
+const {repoId} = useParams();
    
     return(
        
         <div>
+        
          <Navbar></Navbar>
-        <section className="viewrepo">
-            <h2> i am id {repoId}</h2>
+       <div className="viewrepo">
+        {word.map((repo,i) => (
+      <Stack divider={<StackDivider />} spacing='' key={i}>
+        <Box>
+        <Text pt='50' fontSize='sm' color='red'>
+          <Link pt='' to={`/repo/${i}`}>{i == repoId ? repo.name : ''}</Link>
+        </Text>
+
+      <UnorderedList className="list-item">
+      <ListItem className="item">{repo.name}</ListItem>
+      
+      </UnorderedList>
+        
+      </Box>
+      </Stack>
+         ))}
+         
             <Link to='/'>Back to all repository</Link>
-            </section>
+            </div>
             <Footer></Footer>
         </div>
        
