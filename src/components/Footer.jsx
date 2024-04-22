15512c671588f0479eb7262573dd14d70af77840
JOHN OGHENEOCHUKO <ochustech56@gmail.com>
2024-04-22 22:47:05 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                     
                     <p>2024  All rights reserved</p>
                     
-                    <button><a href='https://github-repo-4pfu3oj1v-johns-projects-31a3c5c2.vercel.app/test'>Test 404</a> </button>
+                    <button><a href='/test'>Test 404</a> </button>
                     <button><a href='https://github-repo-4pfu3oj1v-johns-projects-31a3c5c2.vercel.app/error'>Error Boundry</a> </button>
                     <span className='line'></span>
                 </div>
